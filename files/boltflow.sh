8a59a1bb84f2679322c62e4c8ac02e460c5d482b
Bob den Otter <bobdenotter@gmail.com>
2017-02-16 18:54:50 +0100

    Update boltflow.sh

--- files/boltflow.sh
+++ files/boltflow.sh
@@ -17,9 +17,9 @@ elif [[ $PUBLICFOLDER = "" ]] ; then
 fi
 
 if [[ $1 = "update" ]] ; then
-    COMPOSERCOMMAND="update"
+    COMPOSERCOMMAND="update --ignore-platform-reqs --no-dev"
 else
-    COMPOSERCOMMAND="install"
+    COMPOSERCOMMAND="install --no-dev"
 fi
 
 if [[ $1 = "selfupdate" ]] ; then
@@ -52,6 +52,8 @@ if [[ ! -f "$WD/composer.json" ]] ; then
     mv $WD/composer.json.dist $WD/composer.json
 fi
 
+git config core.fileMode false
+
 if ! (git pull) then
     echo "\n\nGit pull was not successful. Fix what went wrong, and run this script again.\n\n"
     exit 1
@@ -64,13 +66,12 @@ fi
 mkdir -p app/database app/cache extensions/ $PUBLICFOLDER/files/ $PUBLICFOLDER/thumbs/
 chmod -Rf 777 $PUBLICFOLDER/files/ $PUBLICFOLDER/theme/ $PUBLICFOLDER/thumbs/
 chmod -Rf 777 app/database/ app/cache/ app/config/ extensions/
-git config core.fileMode false
 
 if [[ ! -f "$WD/composer.phar" ]] ; then
     curl -sS https://getcomposer.org/installer | php
 fi
 
-php composer.phar $COMPOSERCOMMAND --ignore-platform-reqs --no-dev
+php composer.phar $COMPOSERCOMMAND --no-dev
 
 if [[ -f "$WD/extensions/composer.json" ]] ; then
     cd extensions
