fa501a821acac5e1ee047f04e2bfb6ec5899533e
Bob den Otter <bobdenotter@gmail.com>
2018-05-28 15:30:30 +0200

    Updating Bolt.

--- public/theme/bolt-tufte-theme/source/gulpfile.js
+++ public/theme/bolt-tufte-theme/source/gulpfile.js
@@ -5,14 +5,39 @@ var argv = require('yargs').argv;
 // Check for --production flag
 var PRODUCTION = !!(argv.production);
 
+// Browsers to target when prefixing CSS.
+var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
+
 // Define base paths for Sass and Javascript.
-var sassPaths = [
-    'scss/',
-];
+// File paths to various assets are defined here.
+var PATHS = {
+  sass: [
+    'node_modules',
+  ]
+};
 
 var javascriptFiles = [
-    'node_modules/jquery/dist/jquery.js',
+  'javascript/tufte.js',
+  'node_modules/jquery/dist/jquery.js',
+  // 'node_modules/baguettebox.js/src/baguetteBox.js',
+  'node_modules/prismjs/prism.js',
+  'node_modules/prismjs/components/prism-apacheconf.js',
+  'node_modules/prismjs/components/prism-bash.js',
+  'node_modules/prismjs/components/prism-css.js',
+  'node_modules/prismjs/components/prism-json.js',
+  'node_modules/prismjs/components/prism-markdown.js',
+  'node_modules/prismjs/components/prism-php.js',
+  'node_modules/prismjs/components/prism-python.js',
+  'node_modules/prismjs/components/prism-sass.js',
+  'node_modules/prismjs/components/prism-twig.js',
+  'node_modules/prismjs/components/prism-yaml.js',
+  'node_modules/prismjs/components/prism-markup-templating.js',
+  'node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js',
+  'node_modules/prismjs/plugins/line-highlight/prism-line-highlight.js',
+  'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 ];
+
+
 
 // Set up 'sass' task.
 gulp.task('sass', function() {
@@ -20,11 +45,11 @@ gulp.task('sass', function() {
   return gulp.src('scss/tufte.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass({
-      includePaths: sassPaths
+      includePaths: PATHS.sass
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
+      browsers: COMPATIBILITY
     }))
     .pipe($.if(PRODUCTION, $.cssnano()))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
@@ -33,15 +58,10 @@ gulp.task('sass', function() {
 
 // Set up 'compress' task.
 gulp.task('compress', function() {
-  return gulp.src('javascript/*.js')
+  return gulp.src(javascriptFiles)
     .pipe($.if(PRODUCTION, $.uglify()))
+    .pipe($.concat('tufte.js'))
     .pipe(gulp.dest('../javascript'));
-});
-
-
-gulp.task('copyjavascript', function() {
-   gulp.src(javascriptFiles)
-   .pipe(gulp.dest('javascript'));
 });
 
 gulp.task('setproduction', function() {
@@ -49,10 +69,10 @@ gulp.task('setproduction', function() {
 });
 
 // Set up 'default' task, with watches.
-gulp.task('default', ['copyjavascript', 'sass', 'compress'], function() {
+gulp.task('default', ['compress', 'sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch(['javascript/**/*.js'], ['compress']);
 });
 
 // Set up 'build' task, without watches and force 'production'.
-gulp.task('build', ['setproduction', 'copyjavascript', 'sass', 'compress']);
+gulp.task('build', ['setproduction', 'compress', 'sass']);
